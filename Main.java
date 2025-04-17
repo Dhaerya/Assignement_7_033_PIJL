202c13b785f9350e2ba64240a60109243569eacf
Dhaerya <dhaeryamore@gmail.com>
2025-04-17 19:56:57 +0530

    Created Main.java with class and main method structure

---
+++ Main.java
@@ -0,0 +1,13 @@
+// Name: Dhaerya More 
+// PRN: 23070126033
+// Batch: AIML A2
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+    }
+}
