b11f1a54e69167c7009737409f4a301e3d77437c
Dhaerya <dhaeryamore@gmail.com>
2025-04-17 20:03:39 +0530

    Created Calculator.java with add, subtract, and multiply methods

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+//Calculator.java
+// Name: Dhaerya More 
+// PRN: 23070126033
+// Batch: AIML A2
+
+public class Calculator {
+    // Function to add two numbers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Function to subtract two numbers
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Function to multiply two numbers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
